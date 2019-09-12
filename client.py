99667d8dd0dbde3f74d76627503aed3b4a3ae895
Javier Abarca <27842226+Jaleab@users.noreply.github.com>
2019-09-11 22:50:04 -0600

    Update client.py
    Ahora sirve con el server.py (recibe el paquete que mando con los datos modificados a "Buey") y usa correctamente el Random ID (antes usaba ACK y el Random ID solo se enviaba)

--- client.py
+++ client.py
@@ -7,18 +7,21 @@ import random
 import string
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 UDP_IP = "192.168.0.11" #"10.1.138.34"
 UDP_PORT = 5000
-packer1 = struct.Struct('1s f 4s 1s 1s') #  Hacer casos de los datos segun tipo 
+sock2.bind((UDP_IP, 5005))
+packer1 = struct.Struct('1s f 4s 1s 7s') #  Hacer casos de los datos segun tipo 
 packer2 = struct.Struct('1s f 4s 1s f') #  Hacer casos de los datos segun tipo 
-timeCommLimit, timeACKLimit = 1.0, 3.0
+#bueyPack = struct.Struct('1s 4s')
+timeCommLimit, timeRIDLimit = 1.0, 3.0
+ridWanted = ''
 
-def sendPackage(sensorType, data, socket, ackWanted): 
-	values = ((random.choice(string.ascii_letters)).encode(), float(time.time()), 
-			bytearray([0,1,2,3]), sensorType.encode(), data)
+def sendPackage(sensorType, data, socket): 
+	randomID = random.choice(string.ascii_letters)
+	values = (randomID.encode(), float(time.time()), bytearray([0,1,2,3]), sensorType.encode(), data.encode())
 	print(values)
-	bytesSize = sys.getsizeof(values)//8
-	if sys.getsizeof(data)//8 == 1:
+	if sys.getsizeof(data)//8 == 7:
 		packed_data = packer1.pack(*values)
 		#print(sys.getsizeof(packed_data)//8) # Cantidad de bytes 
 	else: 
@@ -26,43 +29,39 @@ def sendPackage(sensorType, data, socket, ackWanted):
 		#print(sys.getsizeof(packed_data)//8) # Cantidad de bytes 
 	#print('{!r}', format(binascii.hexlify(packed_data))) #hexa #print(values) 
 	#print(packer2.unpack(packed_data))
-	#print(sys.getsizeof(packer2.unpack(packed_data))//8)
 	socket.sendto(packed_data, (UDP_IP, UDP_PORT))
-	return packed_data, ackWanted + bytesSize 
-
-def sendACK(ackWanted):
-	sock.sendto(ackWanted.encode(), (UDP_IP, UDP_PORT))
+	return packed_data, randomID, True
 
 def main():
-	ackWanted, ackReceived = 0, -1
-	timeCommStart, timeACKStart = time.time(), time.time()
-	lastSent, ackWanted = sendPackage(hex(0) ,666.666, sock, ackWanted) # tipo y data viene del sensor
-	sendACK(str(ackWanted))
+	ridReceived = ''  
+	timeCommStart, timeRIDStart = time.time(), time.time()
+	lastSent, ridWanted, waitingReply = sendPackage(hex(0), "Carreta", sock) # tipo y data viene del sensor
 	while True:
-		"""try:
-			data, addr = sock2.recvfrom(11)  # Para recibir ACK del receiver
-			ackReceived = 1
-		except: 
-			ackReceived = 0"""
-		print(ackWanted)
-		ackReceived = int(input("ACK para pruebas: \n")) 
-		if ackReceived == -1 and time.time() - timeACKStart >= timeACKLimit: #timeout, no recibio ACK
-			print("ACK was not received. Resending last packet.") 
-			print(time.time() - timeACKStart)
-			sock.sendto(lastSent, (UDP_IP, UDP_PORT)) # Ya esta empacado, no ocupa casos
-			timeCommStart, timeACKStart = time.time(), time.time()
-			sendACK(str(ackWanted))
-		elif ackReceived > -1 and time.time() - timeCommStart >= timeCommLimit: # Recibio ACK, quitar segunda condicion para cuando ya este implementado con sensor y server
-			if ackReceived == ackWanted and time.time() - timeCommStart >= timeCommLimit:    # Hay posibilidad de ACK duplicados en Sender? Si no es el esperado reenvia y el control es en el receiver
-				print("Packet sent.")
-				print(time.time() - timeCommStart)
-				lastSent, ackWanted = sendPackage(hex(0) , 777.777, sock, ackWanted) # tipo y data viene del sensor
-				timeCommStart, timeACKStart = time.time(), time.time()
-			elif ackWanted != ackReceived: # Resend
-				print("ACK did not match. Resending last packet.") #print(time.time() - timeACKStart)
-				sock.sendto(lastSent, (UDP_IP, UDP_PORT)) # ya esta empacado, no ocupa casos
-				timeCommStart, timeACKStart = time.time(), time.time()			
-				sendACK(str(ackWanted))
+		if waitingReply == True:
+			data, addr = sock2.recvfrom(20)  # Para recibir Buey del receiver
+			unpackedData = packer1.unpack(data)
+			ridReceived = (unpackedData)[0].decode()
+			if ridReceived == ridWanted:
+				print("Recibi buey:", unpackedData)
+				waitingReply = False #, serverReplied = False, True
+		else:
+			if waitingReply and time.time() - timeRIDStart >= timeRIDLimit: #timeout, no recibio RID
+				print("Random ID was not received. Resending last packet.") 
+				print(time.time() - timeRIDStart)
+				sock.sendto(lastSent, (UDP_IP, UDP_PORT)) # Ya esta empacado, no ocupa casos
+				timeCommStart, timeRIDStart = time.time(), time.time()
+				serverReplied = False #waitingReply, serverReplied = True, False
+			elif not waitingReply and time.time() - timeCommStart >= timeCommLimit: # Recibio RID, quitar segunda condicion para cuando ya este implementado con sensor y server
+				if ridReceived == ridWanted:    # Hay posibilidad de RID duplicados en Sender? Si no es el esperado reenvia y el control es en el receiver
+					print("Packet sent.")
+					print(time.time() - timeCommStart)
+					lastSent, ridWanted, waitingReply = sendPackage(hex(0) , "Carreta", sock) # tipo y data viene del sensor
+					timeCommStart, timeRIDStart = time.time(), time.time()
+				elif ridWanted != ridReceived: # Resend
+					print("Random ID did not match. Resending last packet.") #print(time.time() - timeACKStart)
+					sock.sendto(lastSent, (UDP_IP, UDP_PORT)) # ya esta empacado, no ocupa casos
+					timeCommStart, timeRIDStart = time.time(), time.time()
+					waitingReply = True #waitingReply, serverReplied = True, False
 main()
 
 """Protocolo del cliente al servidor	
