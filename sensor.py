7e14b3d497eb2add78f56d334e7576802d2f1957
javier06herrera <javier06herrera@gmail.com>
2019-09-15 04:18:00 +0100

    Ultrasonic sensor routine

--- sensor.py
+++ sensor.py
@@ -1,8 +1,36 @@
 GAIN 1
 
-# Main loop.
-while True:
-	values = [0]*4
-	for i in range(4):
-		values[i] = adc.read_adc(i, gain=GAIN)
-	time.sleep(0.5)
+#values = adc.read_adc(i, gain=GAIN)
+
+import RPi.GPIO as GPIO
+import time
+TRIG = 23
+ECHO = 24
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+
+
+def check_point(length)
+	GPIO.output(TRIG, GPIO.LOW)
+	time.sleep(2)
+	GPIO.output(TRIG, GPIO.HIGH)
+	time.sleep(0.00001)
+	GPIO.output(TRIG, GPIO.LOW)
+	counter=0
+	while True:
+
+		while True:
+			if GPIO.input(ECHO) == GPIO.HIGH:
+				pulso_inicio = time.time()
+				break
+
+		while True:
+			if GPIO.input(ECHO) == GPIO.LOW:
+				pulso_fin = time.time()
+				break
+		duracion = pulso_fin - pulso_inicio
+		distancia = (34300 * duracion) / 2
+		if distancia < length
+			counter= counter + 1
