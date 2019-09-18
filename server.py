e0fb3d733a98f76494894a2cb1f516dcbe867722
Javier Abarca <27842226+Jaleab@users.noreply.github.com>
2019-09-18 00:10:38 -0600

    Cliente con mejor estructura y responde buey

--- server.py
+++ server.py
@@ -3,28 +3,38 @@ import socket
 import struct
 import sys
 
-UDP_IP = "10.1.138.42" #"10.1.138.34"
+UDP_IP = "192.168.0.11"
 UDP_PORT = 5000
 
-#unpacker = struct.Struct('1s f 4s 1s 7s')
-unpacker = struct.Struct('1s f 4s 1s f')
+UDP_IP_CLIENT = "127.0.0.1"  		  # IP Cliente - IP para responder al cliente
+UDP_PORT_SERVER_REPLY = 5005		  # Port para repsonder
+
+
+carretaInt = struct.Struct('1s I 4s 1s I') 
+bueyPack = struct.Struct('1s 4s')
 
 sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock2 = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP					 
+                     socket.SOCK_DGRAM) # UDP				 
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-	data, addr = sock.recvfrom(unpacker.size)
-	print ('received message: {!r}',  format(binascii.hexlify(data)))
-	unpacked_data = unpacker.unpack(data)
-	print ('unpacked: ', unpacked_data)
-	#if(unpacked_data[4].decode() == "Carreta"):
+	data, addr = sock.recvfrom(carretaInt.size)
+	#print ('received message: {!r}',  format(binascii.hexlify(data)))
+	unpackedData = carretaInt.unpack(data)
+	#print ('unpacked: ', unpackedData)
 
-	print(unpacked_data)
-	#newPackage = (unpacked_data[0], unpacked_data[1], unpacked_data[2], unpacked_data[3], b"Buey")
-	#packed_data = unpacker.pack(*newPackage)
-	#print(newPackage)
-		#sock2.sendto(packed_data, (UDP_IP, 5000))
-	#socket.sendto(unpacked_data, (UDP_IP, UDP_PORT))
+	newPackage = (unpackedData[0], unpackedData[2])
+	packedData = bueyPack.pack(*newPackage)
+	print(newPackage)
+	sock.sendto(packedData, (UDP_IP_CLIENT, UDP_PORT_SERVER_REPLY))
+
+"""Protocolo del cliente al servidor	
+Random ID	1 byte
+Date	4 bytes
+SensorID	4 bytes
+Tipo de sensor	1 byte
+Datos	
+	
+Protocolo servidor-cliente	
+Random ID	1 byte
+Sensor ID	4 bytes"""
